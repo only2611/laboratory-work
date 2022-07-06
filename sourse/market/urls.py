135da32650c245df0fa4935d04e534ec503a1948
abaibaiyshbekov <abay95poet@inbox.ru>
2022-07-06 23:04:19 +0600

    этап 2 выполнен. Сделаные главная страница и страницы просмотра товара полностью

---
+++ sourse/market/urls.py
@@ -0,0 +1,13 @@
+
+
+from django.urls import path
+
+from market.views import index_view
+
+
+
+urlpatterns = [
+    path("", index_view, name="index_view"),
+
+
+]
--- sourse/market/views.py
+++ sourse/market/views.py
@@ -1,3 +1,21 @@
+from django.db.models.lookups import LessThan
+from django.shortcuts import render, redirect, get_object_or_404
+# Create your views here.
 from django.shortcuts import render
 
+
 # Create your views here.
+
+from market.models import CATEGORY, Product
+
+
+def index_view(request,):
+    products = Product.objects.filter(balance__gte=1)
+    products = products.order_by("categories", "name")
+    context = {"products": products}
+    return render(request, "index.html", context)
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "product.html", {"product": product})
+

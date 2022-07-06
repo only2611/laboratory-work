47fb91123a89354cd98634d2fb95edd38c2a697c
abaibaiyshbekov <abay95poet@inbox.ru>
2022-07-06 16:21:24 +0600

    Создал несколько примеров товаров в админке. фикстура тоже готова. Данные в таблице отражаются.

--- sourse/market/admin.py
+++ sourse/market/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Register your models here.
+from market.models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'description', 'categories',]
+    list_display_links = ['name']
+    list_filter = ['categories']
+    search_fields = ['categories', 'name']
+    fields = ['name', 'description', 'categories', 'balance', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
--- sourse/market/models.py
+++ sourse/market/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Create your models here.
+CATEGORY = [('milk', 'Молочные'), ('organic', 'Органические'), ('fast_feed', 'Быстрого приготовления'),
+            ('drinks', 'Напитки'),('vegetables', 'Овощи'), ('other', 'Разное')]
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Наименование товара")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Описание товара")
+    categories = models.CharField(max_length=100, choices=CATEGORY, verbose_name="Категория", default="other")
+    balance = models.PositiveIntegerField(verbose_name="Остаток товара")
+    price = models.DecimalField(max_length=100, max_digits=7, decimal_places=2, verbose_name="Цена товара")
+
+
+    def __str__(self):
+        return f"{self.id}.{self.name} - {self.description} - {self.categories}"
+
+
+    class Meta:
+        db_table = "market"
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
